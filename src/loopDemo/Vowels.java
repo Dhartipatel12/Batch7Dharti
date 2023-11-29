2bc8744ceda5d600f9946c1f57ba3960e367302c
Dhartipatel12 <dhartidpatel83@yahoo.com>
2023-11-28 21:16:52 -0600

    adding new files Access1,Access2,Alphabets,AscendingArrays,Vowels,ClassA,ClassB,ClassC,ClassD,ConceptsofOOP

---
+++ src/loopDemo/Vowels.java
@@ -0,0 +1,37 @@
+package loopDemo;
+
+public class Vowels {
+
+/* 3. Write your name and check if there is/are any vowels there.
+      if found print them.
+ */
+
+    public void Vowels() {
+        String Name = "Dharti";
+        String Vowels = "AEIOU";
+        int lengthofname = Name.length();
+            System.out.println(lengthofname);
+
+        char firstletter = Name.charAt(0);
+             System.out.println(firstletter);
+        for (int i = 0; i < Name.length(); i++) {
+            if (Name.charAt(i) == 'a' || Name.charAt(i) == 'e' || Name.charAt(i) == 'i'
+                    || Name.charAt(i) == 'o' || Name.charAt(i) == 'u') {
+                System.out.println(Name.charAt(i) + " is Vowel ");
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Vowels Obj = new Vowels();
+        Obj.Vowels();
+    }
+
+}
+
+
+
+
+
--- src/oop/ConceptofOOP.java
+++ src/oop/ConceptofOOP.java
@@ -2,9 +2,12 @@ package oop;
 
 public class ConceptofOOP {
 
-/* Object Oriented Programing
+/* Object Oriented Programing:
 
     1.Inheritance
+      1.Single inheritance
+      2.Multi Level Inheritance
+      3.heirarchial inheritance
     2.Encapsulation
     3.Abstraction
     4.Interface
